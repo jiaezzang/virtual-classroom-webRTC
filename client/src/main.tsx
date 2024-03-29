3457b3b04c608afbfe14ba694e834552eb3672c8
jiaezzang <visveryver2@gmail.com>
2024-03-29 15:20:57 +0900

    feat : tailwind 적용

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './AppRouter';
 import { version } from '../package.json';
+import './index.css';
 
 console.log('v' + version);
 
