56eeb42d0595bf403f21915cf7cb9658a4f74551
jiaezzang <visveryver2@gmail.com>
2024-04-04 09:49:39 +0900

    design : Home 컴포넌트 디자인

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,3 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+/**
+ * ClassRoom 진입 전 Home 컴포넌트
+ * @returns {JSX.Element} Home 컴포넌트
+ */
 export default function Home() {
-    return <div>Home</div>;
+    const navigate = useNavigate();
+    /**
+     * classRoom으로 이동한다.
+     */
+    const redirectToClassRooom = () => {
+        navigate('/classroom');
+    };
+    return (
+        <div className="bg-blue-100 w-screen h-screen flex justify-center items-center">
+            <button
+                className="bg-gradient-to-br from-blue-300 to-blue-100 w-[300px] h-[80px] rounded-full shadow-xl text-xl text-white font-extrabold"
+                onClick={redirectToClassRooom}
+            >
+                ClassRoom
+            </button>
+        </div>
+    );
 }
