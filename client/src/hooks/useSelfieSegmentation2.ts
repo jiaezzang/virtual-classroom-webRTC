721340b38525edfcbd995b3301c8ce5f04047b27
jiaezzang <visveryver2@gmail.com>
2024-04-23 14:13:58 +0900

    refactor : hook 반환값 updateVideo로 수정

--- client/src/hooks/useSelfieSegmentation2.ts
+++ client/src/hooks/useSelfieSegmentation2.ts
@@ -1,99 +1,140 @@
 import { FilesetResolver, ImageSegmenter, ImageSegmenterResult } from '@mediapipe/tasks-vision';
-import { useCallback, useEffect, useRef } from 'react';
-type TVirtualBackdrop = { video: HTMLVideoElement; canvasRef: React.RefObject<HTMLCanvasElement> };
+import { RefObject, useCallback, useMemo, useRef } from 'react';
 
 /**
- * Video에서 인물 외 요소를 제거하여 Canvas에 그려준다.
+ * MediaStream을 입력 받아, 배경을 크로마키 처리하여 캔버스에 그려주고 캔버스의 스트림을 반환한다.
  */
-export const useSelfieSegmentation2 = ({ video, canvasRef }: TVirtualBackdrop) => {
-    const imageSegmenterRef = useRef<ImageSegmenter | null>(null);
-    const lastWebcamTimeRef = useRef(-1); // lastWebcamTime을 ref로 변경
-    const predictWebcam = useCallback(() => {
-        const ctx = canvasRef.current?.getContext('2d');
+export const useSelfieSegmentation2 = ({ canvasRef }: { canvasRef: RefObject<HTMLCanvasElement> }) => {
+    const bgImageDataRef = useRef<ImageData | null>(null);
+    const isBackground = useRef(false);
+    const localCanvas = useRef<HTMLCanvasElement>(document.createElement('canvas'));
+    const localVideoRef = useRef<HTMLVideoElement>(document.createElement('video'));
+    const remoteVideoRef = useRef<HTMLVideoElement>(document.createElement('video'));
+    // const imageSegmenterRef = useRef<ImageSegmenter | null>(null);
+    const lastWebcamTime = useRef(-1);
+    const gap = useRef<number>(0);
+    const vision = useMemo(async () => FilesetResolver.forVisionTasks('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm'), []);
+    const imageSegmenter = useMemo(
+        async () =>
+            await ImageSegmenter.createFromOptions(await vision, {
+                baseOptions: { modelAssetPath: '/selfie_segmenter.tflite', delegate: 'GPU' },
+                runningMode: 'VIDEO',
+                outputCategoryMask: true,
+                outputConfidenceMasks: false
+            }),
+        [vision]
+    );
 
-        if (!ctx) return;
-        if (video.currentTime === lastWebcamTimeRef.current) {
-            window.requestAnimationFrame(() => predictWebcam());
+    const predictWebcam = useCallback(async () => {
+        const localVideo = localVideoRef.current;
+        const ctx = isBackground.current
+            ? localCanvas.current?.getContext('2d', { willReadFrequently: true })
+            : canvasRef.current?.getContext('2d', { willReadFrequently: true });
+        const canvas = isBackground.current ? localCanvas.current : canvasRef.current;
+        if (!ctx || !canvasRef.current || !canvas) return;
+
+        // 마지막으로 캔버스에 그리기를 수행한 시간과 비디오 시간이 같으면 새로 그릴 필요 없음
+        if (localVideo.currentTime === 0 || lastWebcamTime.current === localVideo.currentTime) {
+            requestAnimationFrame(predictWebcam);
             return;
         }
-        lastWebcamTimeRef.current = video.currentTime;
-        ctx?.clearRect(0, 0, video.videoWidth, video.videoHeight);
-        ctx?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-
-        if (!imageSegmenterRef.current) return;
-
-        const startTimeMs = performance.now();
-        imageSegmenterRef.current.segmentForVideo(video, startTimeMs, handleSegmentForVideo);
-    }, [canvasRef, video]);
-
-    const calculateEdgeSoftness = (maskValue: number) => {
-        let alpha;
-        if (maskValue > 0.5) {
-            alpha = 0;
+        lastWebcamTime.current = localVideo.currentTime;
+        const remoteVideo = remoteVideoRef.current;
+        const width = localCanvas.current.width;
+        const height = localCanvas.current.height;
+        if (remoteVideo.autoplay) {
+            //remote Stream 좌측에 그리기
+            ctx?.drawImage(remoteVideo, (width / 4) * -1 + gap.current, 0, width / 2, height);
+            //local Stream 우측에 그리기
+            ctx?.drawImage(localVideo, width / 4 + gap.current, 0, width / 2, height);
+            gap.current < width / 4 ? (gap.current += 15) : (gap.current = width / 4);
         } else {
-            alpha = (1 - maskValue * 2) * 255;
+            //localVideo 중앙에 그리기
+            ctx?.drawImage(localVideo, width / 4 + gap.current, 0, width / 2, height);
+            gap.current > 0 ? (gap.current -= 15) : (gap.current = 0);
         }
-        return alpha;
-    };
+        if (!isBackground.current || !bgImageDataRef.current) {
+            requestAnimationFrame(predictWebcam);
+            return;
+        }
+        const startTimeMs = performance.now();
+        (await imageSegmenter).segmentForVideo(canvas, startTimeMs, handleSegmentForVideo);
+    }, [canvasRef, imageSegmenter]);
 
     /**
      * 비디오 프레임 내에 있는 이미지 객체를 분할한다.
      */
     const handleSegmentForVideo = useCallback(
         (result: ImageSegmenterResult) => {
-            if (!canvasRef.current) return;
-            const ctx = canvasRef.current.getContext('2d');
-
-            if (!ctx || !video || !video.videoWidth || !video.videoHeight) return;
-
-            const imageData = ctx.getImageData(0, 0, video.videoWidth, video.videoHeight);
             const mask = result.categoryMask?.getAsFloat32Array() ?? [];
-            const pixels = imageData.data;
-
-            for (let i = 0; i < mask.length; i++) {
-                // 마스크 값이 0.5 이하면 인물, 이상이면 배경으로 처리
-                const opacity = calculateEdgeSoftness(mask[i]);
-                pixels[i * 4 + 3] = opacity; // Alpha 채널 조정
+            const maskLength = mask.length;
+            if (!mask || !bgImageDataRef.current) {
+                requestAnimationFrame(predictWebcam);
+                return;
             }
+            // console.log(maskLength);
+            if (!canvasRef.current) return;
+            const localCtx = localCanvas.current.getContext('2d');
+            const ctx = canvasRef.current.getContext('2d');
+            if (!ctx || !localCtx) return;
+            const imageData = localCtx.getImageData(0, 0, localCanvas.current.width, localCanvas.current.height);
+            const pixels = imageData.data;
+            if (isBackground.current && bgImageDataRef.current) {
+                let j = 0;
+                for (let i = 0; i < maskLength; ++i) {
+                    // const color = Math.round(mask[i] * 255.0);
 
-            ctx.putImageData(imageData, 0, 0);
+                    if (mask[i] === 1) {
+                        pixels[j] = bgImageDataRef.current.data[j] ?? 0;
+                        pixels[j + 1] = bgImageDataRef.current.data[j + 1] ?? 0;
+                        pixels[j + 2] = bgImageDataRef.current.data[j + 2] ?? 0;
+                        pixels[j + 3] = bgImageDataRef.current.data[j + 3] ?? 0;
+                    }
+
+                    j += 4;
+                }
+            }
+            const uint8Array = new Uint8ClampedArray(pixels.buffer);
+            const data = new ImageData(uint8Array, canvasRef.current.width, canvasRef.current.height);
+            ctx.putImageData(data, 0, 0);
             window.requestAnimationFrame(predictWebcam);
         },
-        [canvasRef, predictWebcam, video]
+        [canvasRef, predictWebcam]
     );
 
-    /** 초기 진입 설정 */
-    useEffect(() => {
-        /** 설정을 초기화한다. */
-        const initializeSettings = async () => {
-            if (!canvasRef.current) return;
-            const canvas = canvasRef.current;
-            try {
-                const data = await FilesetResolver.forVisionTasks('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm');
-                imageSegmenterRef.current = await ImageSegmenter.createFromOptions(data, {
-                    baseOptions: { modelAssetPath: '/selfie_segmenter.tflite', delegate: 'GPU' },
-                    runningMode: 'VIDEO',
-                    outputCategoryMask: true,
-                    outputConfidenceMasks: false
-                });
+    const updateVideo = ({ localVideo, remoteVideo, src }: { localVideo: HTMLVideoElement; remoteVideo?: HTMLVideoElement; src?: string | null }) => {
+        localVideoRef.current = localVideo;
+        if (remoteVideo) remoteVideoRef.current = remoteVideo;
+        if (canvasRef.current) {
+            // Canvas Size 설정
+            canvasRef.current.width = canvasRef.current.offsetWidth;
+            canvasRef.current.height = canvasRef.current.offsetHeight;
+            localCanvas.current.width = canvasRef.current.offsetWidth;
+            localCanvas.current.height = canvasRef.current.offsetHeight;
+        }
 
-                canvas.width = video.videoWidth;
-                canvas.height = video.videoHeight;
+        if (canvasRef.current && src && src.length > 0) {
+            isBackground.current = src.length > 0;
+            const bgCanvas = document.createElement('canvas');
+            const bgCtx = bgCanvas.getContext('2d');
+            bgCanvas.width = canvasRef.current.width;
+            bgCanvas.height = canvasRef.current.height;
 
-                predictWebcam();
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
+            const bgImage = new Image();
+            bgImage.src = src;
 
-        initializeSettings();
+            bgImage.onload = () => {
+                if (!bgCtx) return;
+                bgCtx.drawImage(bgImage, 0, 0, bgCanvas.width, bgCanvas.height);
+                bgImageDataRef.current = bgCtx.getImageData(0, 0, bgCanvas.width, bgCanvas.height);
+                bgCanvas.remove();
+                bgImage.remove();
+            };
+        } else {
+            isBackground.current = false;
+        }
+        predictWebcam();
+    };
 
-        // clean-up
-        return () => {
-            if (video) {
-                video.pause();
-                video.srcObject = null;
-            }
-        };
-    }, [canvasRef, predictWebcam, video]);
+    return { updateVideo };
 };
